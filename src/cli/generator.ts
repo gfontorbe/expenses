31a7491a5af6735a0e0dbbe582f458b731bc8604
Guillaume Fontorbe <gfontorbe@gmail.com>
2021-11-20 13:08:48 +0100

    Add report command to cli

--- src/cli/generator.ts
+++ src/cli/generator.ts
@@ -1,24 +1,94 @@
-import fs from 'fs';
-import { CompositeGeneratorNode, NL, processGeneratorNode } from 'langium';
-import { Model } from '../language-server/generated/ast';
-import { extractDestinationAndName } from './cli-util';
-import path from 'path';
+//import fs from "fs";
+//import { CompositeGeneratorNode, NL, processGeneratorNode } from "langium";
+import { Expense, Income, Model } from "../language-server/generated/ast";
+// import { extractDestinationAndName, getFileName, COLOR } from "./cli-util";
+import {getFileName, COLOR } from "./cli-util";
+//import path from "path";
 
-export function generateJavaScript(model: Model, filePath: string, destination: string | undefined): string {
-    const data = extractDestinationAndName(filePath, destination);
-    const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
+// export function generateJavaScript(
+//     model: Model,
+//     filePath: string,
+//     destination: string | undefined
+// ): string {
+//     const data = extractDestinationAndName(filePath, destination);
+//     const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
 
-    const fileNode = new CompositeGeneratorNode();
-    fileNode.append('"use strict";', NL, NL);
-    model.expenses.forEach(greeting => fileNode.append(`console.log('Hello, ${greeting.amount}!');`, NL));
+//     const fileNode = new CompositeGeneratorNode();
+//     fileNode.append('"use strict";', NL, NL);
+//     model.expenses.forEach((greeting) =>
+//         fileNode.append(`console.log('Hello, ${greeting.amount}!');`, NL)
+//     );
 
-    if (!fs.existsSync(data.destination)) {
-        fs.mkdirSync(data.destination, { recursive: true });
-    }
-    fs.writeFileSync(generatedFilePath, processGeneratorNode(fileNode));
+//     if (!fs.existsSync(data.destination)) {
+//         fs.mkdirSync(data.destination, { recursive: true });
+//     }
+//     fs.writeFileSync(generatedFilePath, processGeneratorNode(fileNode));
 
-    model.expenses.forEach(x => console.log(x.amount));
+//     model.expenses.forEach((x) => console.log(x.amount));
 
-    return generatedFilePath;
+//     return generatedFilePath;
+// }
+
+export function generateReport(
+    model: Model,
+    filePath: string,
+    destination?: string
+): void {
+    const fileName = getFileName(filePath);
+    console.log(`Generating report for ${fileName}... \n`);
+
+    console.log(
+        `Found ${model.expenses.length} expense entr${model.expenses.length > 0 ? "ies" : "y"
+        }`
+    );
+    console.log(
+        `Found ${model.incomes.length} income entr${model.incomes.length > 0 ? "ies" : "y"
+        }`
+    );
+    console.log();
+
+    console.log("Expenses: ");
+    logEntries(model.expenses);
+    console.log(`${COLOR.fgRed}Total expenses: ${sumOfPayments(model.expenses).toFixed(2)}eur${COLOR.reset} \n`);
+
+    console.log("Incomes: ");
+    logEntries(model.incomes);
+    console.log(`${COLOR.fgGreen}Total expenses: ${sumOfPayments(model.incomes).toFixed(2)}eur${COLOR.reset} \n`);
+
+    console.log(`Balance: `);
+    let balance = getBalance(model);
+    console.log(`${balance >= 0 ? COLOR.fgGreen : COLOR.fgRed}${balance.toFixed(2)}eur${COLOR.reset}`);
 }
 
+function logEntries(entries: Income[] | Expense[]): void {
+
+    if (entries.length === 0) {
+        console.log(`No entry found.`);
+    } else {
+        entries.sort((e1, e2) => {
+            if (e1.paymentDate > e2.paymentDate) {
+                return 1;
+            } else if (e1.paymentDate < e2.paymentDate) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+
+        entries.forEach((entry) => 
+            console.log(`${entry.paymentDate} ${entry.amount.toFixed(2)}eur ${entry.tag}`)        
+        );
+    }
+}
+
+function sumOfPayments(entries: Income[] | Expense[]): number {
+    let sum = 0;
+    entries.forEach((entry) => 
+    sum += entry.amount );
+
+    return sum;
+}
+
+function getBalance(model: Model): number{
+    return sumOfPayments(model.incomes) - sumOfPayments(model.expenses);
+}
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,16 +1,23 @@
-import colors from 'colors';
+// import colors from 'colors';
 import { Command } from 'commander';
 import { languageMetaData } from '../language-server/generated/module';
 import { Model } from '../language-server/generated/ast';
 import { createExpensesDslServices } from '../language-server/expenses-dsl-module';
 import { extractAstNode } from './cli-util';
-import { generateJavaScript } from './generator';
+// import { generateJavaScript, generateReport } from './generator';
+import { generateReport } from './generator';
 
-export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
+// export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
+//     const model = await extractAstNode<Model>(fileName, languageMetaData.fileExtensions, createExpensesDslServices());
+//     const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
+//     console.log(colors.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+// };
+
+export const generateReportAction = async (fileName: string, options: GenerateOptions) : Promise<void> => {
     const model = await extractAstNode<Model>(fileName, languageMetaData.fileExtensions, createExpensesDslServices());
-    const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
-    console.log(colors.green(`JavaScript code generated successfully: ${generatedFilePath}`));
-};
+
+    generateReport(model, fileName, undefined);
+}
 
 export type GenerateOptions = {
     destination?: string;
@@ -23,12 +30,18 @@ export default function(): void {
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         .version(require('../../package.json').version);
 
+    // program
+    //     .command('generate')
+    //     .argument('<file>', `possible file extensions: ${languageMetaData.fileExtensions.join(', ')}`)
+    //     .option('-d, --destination <dir>', 'destination directory of generating')
+    //     .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+    //     .action(generateAction);
+
     program
-        .command('generate')
+        .command('report')
         .argument('<file>', `possible file extensions: ${languageMetaData.fileExtensions.join(', ')}`)
-        .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
-        .action(generateAction);
+        .description('generates report for expenses registered in the file')
+        .action(generateReportAction);
 
     program.parse(process.argv);
 }
